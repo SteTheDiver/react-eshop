fd0efbb4cf9058e1a8f6cfff3da6f8b8232b9f45
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-05-15 17:06:54 +0200

    Created pages

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import Cart from "./CartPage";
+import About from "./AboutPage";
+import Error from "./ErrorPage";
+import SingleProduct from "./SingleProductPage";
+import Products from "./ProductsPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {Home, Cart, About, Error, SingleProduct, Products, Checkout, PrivateRoute};
