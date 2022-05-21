fb748f7efff9cb244599dee3bb5259360eb2a057
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-05-21 09:27:32 +0200

    Udpated with new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,25 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
+import PageHero from "./PageHero";
+import Hero from "./Hero";
+import FeaturedProducts from "./FeaturedProducts";
+import Contact from "./Contact";
+import Services from "./Services";
+import Loader from "./Loader";
+import Error from "./Error";
+import Product from "./Product";
 
-export {Navbar, Footer, Sidebar};
+export {
+  Navbar,
+  Footer,
+  Sidebar,
+  Services,
+  PageHero,
+  Hero,
+  FeaturedProducts,
+  Contact,
+  Loader,
+  Error,
+  Product,
+};
