1941fffbf6c798505f18635661115cef842c319e
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-05-22 10:41:12 +0200

    Fetching product and display info

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { useProductsContext } from "../context/products_context";
+import { formatPrice } from "../utils/helpers";
+
+import { single_product_url as url } from "../utils/constants";
+import {
+  Loader,
+  Error,
+  PageHero,
+  ProductImages,
+  AddToCart,
+  Stars,
+} from "../components";
+import styled from "styled-components";
 
 const SingleProductPage = () => {
-  return (
-    <div>SingleProductPage</div>
-  )
-}
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-export default SingleProductPage
+  const {
+    single_product_loading,
+    single_product_error,
+    single_product,
+    fetchSingleProduct,
+  } = useProductsContext();
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+  console.log(single_product);
+
+  useEffect(() => {
+    if (single_product_error) {
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  }, [single_product_error]);
+
+  if (single_product_loading) {
+    return <Loader />;
+  }
+
+  if (single_product_error) {
+    return <Error />;
+  }
+
+  const {
+    id: sku,
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    company,
+    images,
+  } = single_product;
+
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          back to products
+        </Link>
+        <div className="product-center">
+          <ProductImages images={images} />
+          <section className="content">
+            <h2>{name}</h2>
+            <Stars stars={stars} reviews={reviews} />
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="desc">{description}</p>
+            <p className="info">
+              <span>Available:</span>
+              {stock > 0 ? "In stock" : "Out of stock"}
+            </p>
+            <p className="info">
+              <span>SKU:</span>
+              {sku}
+            </p>
+            <p className="info">
+              <span>Brand:</span>
+              {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart product={single_product} />}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  .product-center {
+    display: grid;
+    gap: 4rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: var(--clr-primary-5);
+  }
+  .desc {
+    line-height: 2;
+    max-width: 45em;
+  }
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
+  @media (min-width: 992px) {
+    .product-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+    .price {
+      font-size: 1.25rem;
+    }
+  }
+`;
+
+export default SingleProductPage;
